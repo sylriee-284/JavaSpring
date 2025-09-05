9c8b4598733c17c060c54936931e7de560051477
Sylriee <vietbuiquoc2301@gmail.com>
2025-09-05 12:55:37 +0700

    hoc ham save

--- src/main/java/vn/hoidanit/laptopshop/controller/AdminController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/AdminController.java
@@ -7,9 +7,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class AdminController {
+
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/admin/user/create")
     public String showCreateForm(Model model) {
@@ -22,6 +29,7 @@ public class AdminController {
         if (syl1 != null) {
             System.out.println("User info: " + syl1.getEmail() + " - " + syl1.getFullName() + " - " + syl1.getPhone());
         }
+        this.userService.handleSaveUser(syl1);
         return "admin/user/create";
     }
 
